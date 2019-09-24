e9c6a35645ca375b7890a19e408f8398fe1f22f3
Dan McCormack <cowgod@gmail.com>
2019-09-23 22:08:19 -0400

    Update example runner scripts

--- contrib/EXAMPLE__run.sh
+++ contrib/EXAMPLE__run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export SLACK_API_TOKEN=<xxxxxxx>
 export BUNGIE_API_TOKEN=<xxxxxxx>
 export GUNSMITH_BOT_USERNAME=<xxxxxxx>
+export ENVIRONMENT=production
+export GUNSMITH_DB_HOST=<xxxxxxx>
+export GUNSMITH_DB_USERNAME=<xxxxxxx>
+export GUNSMITH_DB_PASSWORD=<xxxxxxx>
+export GUNSMITH_DB_NAME=<xxxxxxx>
 
-ruby ./app/discord_runner.rb
+export SLACK_API_TOKEN=<xxxxxxx>
+### OR
+###export DISCORD_API_TOKEN=<xxxxxxx>
+
+
+ruby ./app/slack_runner.rb
+### OR
+###ruby ./app/discord_runner.rb
