b4913e5aebc82b4f14c77dd0a161806a68e428ae
Dan McCormack <cowgod@users.noreply.github.com>
2021-10-28 15:02:10 -0400

    Daemonize Twitch-related scripts

--- app/announce_twitch_clips.rb
+++ app/announce_twitch_clips.rb
@@ -3,11 +3,19 @@
 
 require_relative '../environment'
 
+sleep_interval = $config&.dig('twitch_clips', 'announce_interval').presence || 60
 
-Cowgod::Logger.log 'Reporting new clips...'
 
-Bungie::Activities::Clip.pending_announcement.each do |clip|
-  clip.announce if clip&.tracked_players
+loop do
+  Cowgod::Logger.log 'Reporting new clips...'
+
+  Bungie::Activities::Clip.pending_announcement.each do |clip|
+    clip.announce if clip&.tracked_players
+  end
+
+  Cowgod::Logger.log "Done reporting new clips. Sleeping #{sleep_interval} secs..."
+
+
+  sleep sleep_interval
 end
 
-Cowgod::Logger.log 'Done reporting new clips.'
--- app/find_twitch_clips.rb
+++ app/find_twitch_clips.rb
@@ -3,39 +3,45 @@
 
 require_relative '../environment'
 
+sleep_interval = $config&.dig('twitch_clips', 'find_interval').presence || 60
 
-Cowgod::Logger.log 'Importing new activities...'
 
-Bungie::User.where(find_twitch_clips: true)&.each do |bungie_user|
-  ### TODO -- move this logic into Bungie::User
-  bungie_user&.memberships&.each do |membership|
-    membership.load_characters&.values&.each do |character|
-      character.load_unscanned_activities(mode: Bungie::Api::ACTIVITY_MODES[:all_pvp])&.each_value do |activity|
-        # character.load_unscanned_activities(mode: Bungie::Api::ACTIVITY_MODES[:trials_of_osiris])&.each_value do |activity|
+loop do
+  Cowgod::Logger.log 'Importing new activities...'
 
-        activity.players&.with_twitch_account&.each do |player|
-          twitch_user = player.bungie_user&.load_twitch_user
-          next unless twitch_user
+  Bungie::User.where(find_twitch_clips: true)&.each do |bungie_user|
+    ### TODO -- move this logic into Bungie::User
+    bungie_user&.memberships&.each do |membership|
+      membership.load_characters&.values&.each do |character|
+        character.load_unscanned_activities(mode: Bungie::Api::ACTIVITY_MODES[:all_pvp])&.each_value do |activity|
+          # character.load_unscanned_activities(mode: Bungie::Api::ACTIVITY_MODES[:trials_of_osiris])&.each_value do |activity|
 
-          twitch_user.load_videos.each do |video|
-            next unless video.contains_activity(activity)
+          activity.players&.with_twitch_account&.each do |player|
+            twitch_user = player.bungie_user&.load_twitch_user
+            next unless twitch_user
 
-            log_msg = "Found clip for user '#{bungie_user.bungie_name}' "
-            log_msg += "from Twitch user #{twitch_user.display_name} "
-            log_msg += "on #{activity.started_at}"
-            Cowgod::Logger.log log_msg
+            twitch_user.load_videos.each do |video|
+              next unless video.contains_activity(activity)
 
-            Bungie::Activities::Clip.find_or_create_by(activity: activity, twitch_video: video)
+              log_msg = "Found clip for user '#{bungie_user.bungie_name}' "
+              log_msg += "from Twitch user #{twitch_user.display_name} "
+              log_msg += "on #{activity.started_at}"
+              Cowgod::Logger.log log_msg
+
+              Bungie::Activities::Clip.find_or_create_by(activity: activity, twitch_video: video)
+            end
           end
-        end
 
-        # Mark activity as scanned
-        activity.scanned_at = Time.now
-        activity.save
+          # Mark activity as scanned
+          activity.scanned_at = Time.now
+          activity.save
+        end
       end
     end
   end
+
+  Cowgod::Logger.log "Done importing new activities. Sleeping #{sleep_interval} secs..."
+
+
+  sleep sleep_interval
 end
-
-Cowgod::Logger.log 'Done importing new activities.'
-
