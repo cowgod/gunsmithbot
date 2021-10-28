63845afc0a56017e3885faa2dfc04a4d5a6b0d2b
Dan McCormack <cowgod@users.noreply.github.com>
2021-10-28 13:53:03 -0400

    Fix relative path specification

--- environment.rb
+++ environment.rb
@@ -9,11 +9,10 @@ end
 
 
 # A global variable isn't my favorite solution, but it'll work for now
-$config = YAML.safe_load(File.read('config/config.yml'))
+$config = YAML.safe_load(File.read(File.expand_path('config/config.yml', __dir__)))
 
 
-
-OTR::ActiveRecord.configure_from_file! 'config/database.yml'
+OTR::ActiveRecord.configure_from_file! File.expand_path('config/database.yml', __dir__)
 OTR::ActiveRecord.establish_connection!
 
 
