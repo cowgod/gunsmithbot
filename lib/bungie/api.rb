2d2bb847e0ab407a308c05ace5e01cafff7a80c4
Dan McCormack <cowgod@users.noreply.github.com>
2022-03-07 22:32:51 -0500

    Support formatting item objectives (like crafting date)

--- lib/bungie/api.rb
+++ lib/bungie/api.rb
@@ -7,6 +7,7 @@ require 'time'
 require 'tempfile'
 require 'sqlite3'
 require 'zip'
+require 'date'
 
 require_relative '../concerns/retryable'
 
@@ -251,6 +252,61 @@ module Bungie
       Void:    4,
       Raid:    5,
       Stasis:  6
+    }.freeze
+
+    # DestinyUnlockValueUIStyle
+    UI_STYLES = {
+      # Generally, Automatic means "Just show the number"
+      Automatic: 0,
+
+      # Show the number as a fractional value. For this to make sense, the value being displayed should have a
+      # comparable upper bound, like the progress to the next level of a Progression.
+      Fraction: 1,
+
+      # Show the number as a checkbox. 0 Will mean unchecked, any other value will mean checked.
+      Checkbox: 2,
+
+      # Show the number as a percentage. For this to make sense, the value being displayed should have a comparable
+      # upper bound, like the progress to the next level of a Progression.
+      Percentage: 3,
+
+      # Show the number as a date and time. The number will be the number of seconds since the Unix Epoch (January 1st,
+      # 1970 at midnight UTC). It'll be up to you to convert this into a date and time format understandable to the user
+      # in their time zone.
+      DateTime: 4,
+
+      # Show the number as a floating point value that represents a fraction, where 0 is min and 1 is max. For this to
+      # make sense, the value being displayed should have a comparable upper bound, like the progress to the next level
+      # of a Progression.
+      FractionFloat: 5,
+
+      # Show the number as a straight-up integer.
+      Integer: 6,
+
+      # Show the number as a time duration. The value will be returned as seconds.
+      TimeDuration: 7,
+
+      # Don't bother showing the value at all, it's not easily human-interpretable, and used for some internal purpose.
+      Hidden: 8,
+
+      # Example: "1.5x"
+      Multiplier: 9,
+
+      # Show the value as a series of green pips, like the wins in a Trials of Osiris score card.
+      GreenPips: 10,
+
+      # Show the value as a series of red pips, like the losses in a Trials of Osiris score card.
+      RedPips: 11,
+
+      # Show the value as a percentage. For example: "51%" - Does no division, only appends '%'
+      ExplicitPercentage: 12,
+
+      # Show the value as a floating-point number. For example: "4.52" NOTE: Passed along from Investment as whole
+      # number with last two digits as decimal values (452 -> 4.52)
+      RawFloat: 13,
+
+      # Show the value as a level and a reward.
+      LevelAndReward: 14,
     }.freeze
 
     # The value of the uiCategoryStyle for socketCategories that we want to
@@ -623,7 +679,7 @@ module Bungie
             if objective&.dig('complete')
               item_details[:objectives] += [{
                                               label: objective_definition&.dig('progressDescription'),
-                                              value: objective&.dig('progress')
+                                              value: Bungie::Api.format_objective_value(objective&.dig('progress'), objective_definition&.dig('completedValueStyle'))
                                             }]
             end
           end
@@ -985,6 +1041,19 @@ module Bungie
     def self.get_hex_color_for_damage_type(damage_type)
       damage_type_key = damage_type.downcase.to_sym
       DAMAGE_COLOR[damage_type_key] || DAMAGE_COLOR[:kinetic]
+    end
+
+
+    def self.format_objective_value(value, ui_style)
+      case ui_style
+      when self::UI_STYLES[:DateTime]
+        # Show the number as a date and time. The number will be the number of seconds since the Unix Epoch (January 1st,
+        # 1970 at midnight UTC). It'll be up to you to convert this into a date and time format understandable to the user
+        # in their time zone.
+        Time.at(value).to_datetime.strftime("%Y-%m-%d")
+      else
+        value&.to_formatted_s
+      end
     end
 
 
